462a28a6d81e03379d5e73c5b114a63bc8ff81af
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2019-12-11 14:30:02 +0000

    autoclean function

--- MediaCenter/src/Client/Client.java
+++ MediaCenter/src/Client/Client.java
@@ -2,14 +2,40 @@ package Client;
 
 import MediaCenter_GUI.LoginForm;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Client {
     private static MediaCenterInterface mci;
 
     public static void main(String[] args) throws InterruptedException, IOException {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                File folder = new File("src/Client/.reproduction/");
+                deleteFolder(folder);
+            }
+        }));
+
         mci = new RemoteMediaCenter("localhost", 16899);
 
         LoginForm lf = new LoginForm(mci);
     }
+
+    public static void deleteFolder(File folder) {
+        File[] files = folder.listFiles();
+
+        System.out.println(files.length);
+        if(files!=null) { //some JVMs return null for empty dirs
+            for(File f: files) {
+                System.out.println(f.getName());
+                if(f.isDirectory()) {
+                    deleteFolder(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+
 }
